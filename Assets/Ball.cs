825984709bdb114ff7a7ee1e5b696a75a5547a2d
Martyna Łagożna <m.lagozna@live.com>
2017-06-04 16:37:07 +0200

    finished game

---
+++ Assets/Ball.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour {
+
+    public Rigidbody2D rigidbody;
+    public Rigidbody2D hook;
+
+    public float releaseTime = .15f;
+    public float maxDragDistane = 2f;
+
+    private bool isFlying = false;
+    private bool isPaused = false;
+    private bool isPressed = false;
+
+    public GameObject nextBall;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 0 : 1;
+        }
+
+        if (!isFlying && !isPaused && isPressed) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector3.Distance(mousePosition, hook.position) > maxDragDistane) 
+                rigidbody.position = hook.position + (mousePosition - hook.position).normalized * maxDragDistane;         
+            else 
+                rigidbody.position = mousePosition;                  
+        }
+    }
+
+    private void OnMouseDown() {
+        if (isFlying)
+            return;
+
+        isPressed = true;
+        rigidbody.isKinematic = true;        
+    }
+
+    private void OnMouseUp() {
+        if (isFlying)
+            return;
+
+        isPressed = false;
+        rigidbody.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release() {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        isFlying = true;
+
+        yield return new WaitForSeconds(2f);
+        if (nextBall != null)
+            nextBall.SetActive(true);
+        else {
+            Enemy.enemiesAlive = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject deathEffect;
+    public float health = 4f;
+    public static int enemiesAlive = 0;
+    public string nextLevel = "Level 2";
+    public bool enableToSwitchLevel;
+    public Text levelCompletedText;
+    public GameObject NextLevelButton;
+    public AudioSource audio;
+
+    void Start() {
+        enemiesAlive++;
+        enableToSwitchLevel = false;
+        levelCompletedText.text = "";
+    }
+
+    private void OnCollisionEnter2D(Collision2D colInfo) {
+        if (colInfo.relativeVelocity.magnitude > health) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Destroy(gameObject);
+        enemiesAlive--;
+        audio.Play();
+        Debug.Log("Enemy died, enemies alive: " + enemiesAlive.ToString());
+        if (enemiesAlive <= 0) {
+            levelCompletedText.text = "Level completed!";
+            Win();
+        }
+
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+    }
+
+    public void Win() {
+        levelCompletedText.text = "Level completed!";
+        enableToSwitchLevel = true;
+        NextLevelButton.gameObject.SetActive(true);
+        if (nextLevel.Equals("Level 2")) {
+            Application.Quit();
+        }
+    }
+}
